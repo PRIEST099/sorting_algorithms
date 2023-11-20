cb7fe472ac093e36629ef262e109405b10059b67
Ahadi <ahadic044@gmail.com>
2023-11-20 23:17:15 +0200

    implemented shell sort and cocktail sort algorithms

--- sort.h
+++ sort.h
@@ -1,7 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 #include <stddef.h>
-
+#include <stdbool.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -22,4 +22,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+
 #endif
